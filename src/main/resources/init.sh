8d13b451a66517cddfce3d0dd9be3946f671f969
shtykh <shtykh@yandex.ru>
2015-03-03 03:41:22 +0300

    Update init.sh

--- src/main/resources/init.sh
+++ src/main/resources/init.sh
@@ -1,9 +1,2 @@
 #!/bin/sh
-psql postgres -c "CREATE USER shtykhnekki_user WITH PASSWORD 'shtykhnekki' SUPERUSER"
-psql postgres -c "CREATE DATABASE shtykhnekki OWNER shtykhnekki_user"
-psql shtykhnekki -c "CREATE TABLE IF NOT EXISTS entry  (
-	id UUID NOT NULL,
-	content VARCHAR(1024) NOT NULL,
-	creationDate DATE NOT NULL,
-	PRIMARY KEY (id)
-)"
+psql postgres -f init.sql
