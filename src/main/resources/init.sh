cfe13c035a473340002d7a092bc434fe47c224f4
Alexey Shtykh <shtykh.alexey@gmail.com>
2015-03-03 09:56:21 +0300

    Revert "Update init.sh"
    This reverts commit 8d13b451a66517cddfce3d0dd9be3946f671f969.

--- src/main/resources/init.sh
+++ src/main/resources/init.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
-psql postgres -f init.sql
+psql postgres -c "CREATE USER shtykhnekki_user WITH PASSWORD 'shtykhnekki' SUPERUSER"
+psql postgres -c "CREATE DATABASE shtykhnekki OWNER shtykhnekki_user"
+psql shtykhnekki -c "CREATE TABLE IF NOT EXISTS entry  (
+	id UUID NOT NULL,
+	content VARCHAR(1024) NOT NULL,
+	creationDate DATE NOT NULL,
+	PRIMARY KEY (id)
+)"
